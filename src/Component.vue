0690dc78a389541d840a01e149d7639798e06a12
Sunshine <sunshine.mahant@gmail.com>
2022-03-18 10:17:18 +0530

    wip

--- src/Component.vue
+++ src/Component.vue
@@ -4,11 +4,37 @@
         
         <br/>
 
-        <input class="input" type="input" v-model="text">
-        
-        <br/>
-        
-        <button class="button-primary" @click="save">Save</button>
+      <div class="mt-5">
+        <h3 class="block font-bold mb-2">Packages</h3>
+
+        <table class="table-auto w-full">
+          <thead>
+            <tr>
+              <th class="px-3 py-1 border-none text-left">Name</th>
+              <th class="px-3 py-1 text-left">Version</th>
+              <th class="px-3 py-1 text-left">Description</th>
+              <th class="px-3 py-1 text-left"></th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr v-for="(package, index) in pluginData.packages" :key="index">
+            <td class="px-3 py-2">
+              <span v-model="pluginData.packages[index].name" />
+            </td>
+            <td class="px-3 py-2">
+              <span v-model="pluginData.packages[index].version" />
+            </td>
+            <td class="px-3 py-2">
+              <span v-model="pluginData.packages[index].description" />
+            </td>
+            <td>
+<!--              <button class="button-danger" @click="removeTenant(index)">Remove</button>-->
+              <input type="checkbox" :id="pluginData.packages[index].package" :name="pluginData.packages[index].name" :value="index">
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -8,8 +8,24 @@ module.exports = (vemto) => {
 
         onInstall() {
             vemto.savePluginData({
-                text: 'Hello world!!'
+                packages: [
+                    {name: 'Arcanedev Laravel Notes', package: 'arcanedev/laravel-notes', version: '^9.0', checked: true, description: 'Arcanedev Laravel Notes is a package that allows you to create notes for your Laravel projects.', link: 'https://github.com/ARCANEDEV/LaravelNotes'},
+                    {name: 'Cybercog Laravel Love', package: 'cybercog/laravel-love', version: '^8.9', checked: true, description: 'Add favourites to your models', link: 'https://github.com/cybercog/laravel-love'},
+                    {name: 'Cybercog Laravel Ban', package: 'cybercog/laravel-ban', version: '^4.7', checked: true, description: 'Add bans to your models', link: 'https://github.com/cybercog/laravel-ban'},
+                ]
             })
+        },
+
+        composerPackages(packages) {
+            let data = vemto.getPluginData()
+
+            for (let package of data.packages) {
+                if (package.checked) {
+                    packages.require[package.package] = package.version
+                }
+            }
+
+            return packages
         },
 
         beforeCodeGenerationStart() {
